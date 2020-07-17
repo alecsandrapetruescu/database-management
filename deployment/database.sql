a44b58cf5ed899cd97a98710af9b919e6c620f74
Alecsandra Petruescu <alecsandra.petruescu@gmail.com>
2020-07-17 09:11:45 +0300

    [update] adjusted db

--- deployment/database.sql
+++ deployment/database.sql
@@ -1,11 +1,20 @@
-DROP DATABASE IF EXISTS user_management;
-CREATE DATABASE IF NOT EXISTS user_management CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+DROP DATABASE IF EXISTS `users`;
+CREATE DATABASE IF NOT EXISTS `users` CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user`
+USE `users`;
+DROP TABLE IF EXISTS `users`.`users`;
+CREATE TABLE IF NOT EXISTS `users`.`users`
 (
     id BIGINT(50) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci null,
     first_name VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci null,
     last_name VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci null
 ) ENGINE = InnoDB;
+
+INSERT INTO `users`.`users` (id, username, first_name, last_name) VALUES
+(1, "user1", "jon", "doe"),
+(2, "user2", "jane", "doe"),
+(3, "user3", "jack", "doe"),
+(4, "user4", "janet", "doe");
+
+SELECT * FROM `users`.`users`;
